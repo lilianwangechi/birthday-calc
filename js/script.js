27b805d0670e1835ab94d02e0f80f3b9a97c772e
Lilianwangechi <lilian.wangechi@student.moringaschool.com>
2021-12-13 11:48:11 +0300

    make changes to the names of female variables reading male

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* male */
 
+
 var namesMale = ["Kwasi"," Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var weekNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
 
@@ -11,7 +12,7 @@ function birthDay1(cc,yy,mm,dd){
   var yy = document.getElementById("yearofbirth").value;
   
   const BIRTHDAY = ((((cc/4)- 2 * cc -1) + ((5 * yy /4)) + ((5 * yy/4 )) + ((26 * (mm + 1 )/ 10)) + dd)%7).toFixed(0);
-  if (BIRTHDAY == 0) {alert(namesMale[0] + "is your Ghanian name.You were born on + "weekNames[0]")
+  if (BIRTHDAY == 0) {alert(namesFemale[0] + "is your Ghanian name.You were born on + "weekNames[0]")
   }else if (BIRTHDAY == 1){
   alert(namesMale[0] + " is your Ghanian name.You were born on  + "weekNames[1]")
   }else if (BIRTHDAY == 2){
@@ -30,40 +31,30 @@ function birthDay1(cc,yy,mm,dd){
 
 
 
-
-
-
-
-
-
-
-
-
-
 /* female */
 var namesFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
-function birthDay1(cc,yy,mm,dd){
+function birthDay1(cc1,yy1,mm1,dd1){
    
-  var dd = document.getElementById("dateofbirth").value; 
-  var mm = document.getElementById("monthofbirth").value;
-  var cc = document.getElementById("centuryofbirth").value;
-  var yy = document.getElementById("yearofbirth").value;
+  var dd1 = document.getElementById("dateofbirth").value; 
+  var mm1 = document.getElementById("monthofbirth").value;
+  var cc1 = document.getElementById("centuryofbirth").value;
+  var yy1 = document.getElementById("yearofbirth").value;
   
 
-  const BIRTHDAY = ((((cc/4)- 2 * cc -1) + ((5 * yy /4)) + ((5 * yy/4 )) + ((26 * (mm + 1 )/ 10)) + dd)%7).toFixed(0);
-if (BIRTHDAY == 0) {alert(namesMale[0] + "is your Ghanian name.You were born on + "weekNames[0]")
+  const BIRTHDAY = ((((cc1/4)- 2 * cc1 -1) + ((5 * yy1 /4)) + ((5 * yy1/4 )) + ((26 * (mm1 + 1 )/ 10)) + dd1)%7).toFixed(0);
+if (BIRTHDAY == 0) {alert(namesFemale[0] + "is your Ghanian name.You were born on + "weekNames[0]")
 }else if (BIRTHDAY == 1){
-alert(namesMale[0] + " is your Ghanian name.You were born on  + "weekNames[1]")
+alert(namesFemale[0] + " is your Ghanian name.You were born on  + "weekNames[1]")
 }else if (BIRTHDAY == 2){
-ralert(namesMale[0] + " is your Ghanian name.You were born on  + "weekNames[2]")
+ralert(namesFemale[1] + " is your Ghanian name.You were born on  + "weekNames[2]")
 }else if (BIRTHDAY == 3){
-alert(namesMale[0] + " is your Ghanian name.You were born on  + "weekNames[3]")
+alert(namesFemale[2] + " is your Ghanian name.You were born on  + "weekNames[3]")
 }else if (BIRTHDAY == 4){
-alert(namesMale[0] + " is your Ghanian name.You were born on  + "weekNames[4]")
+alert(namesFemale[3] + " is your Ghanian name.You were born on  + "weekNames[4]")
 }else if (BIRTHDAY == 5){
-alert(namesMale[0] + " is your Ghanian name.You were born on  + "weekNames[5]")
+alert(namesFemale[4] + " is your Ghanian name.You were born on  + "weekNames[5]")
 }else if (BIRTHDAY == 6){
-alert(namesMale[0] + "  is your Ghanian name.You were born on  + "weekNames[6]")
+alert(namesFemale[6] + "  is your Ghanian name.You were born on  + "weekNames[6]")
 }
 }
