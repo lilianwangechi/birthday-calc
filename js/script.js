872afcb5f3f70970c29bf29920bbe5177cbd531a
Lilianwangechi <lilian.wangechi@student.moringaschool.com>
2021-12-13 11:30:16 +0300

    add arrays for name

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/* male */
+
+var namesMale = ["Kwasi"," Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var weekNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* female */
+var namsFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+
